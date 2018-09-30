bd1d32d367f42ac71d8493bdc00f2f18ddec041d
Daisuke Matsumoto <sagirikiri@gmail.com>
2018-09-30 20:02:00 +0900

    fix config.

--- TLMedia/resources/views/article/create.blade.php
+++ TLMedia/resources/views/article/create.blade.php
@@ -1,5 +1,12 @@
 @extends('app.layout')
 
 @section('content')
-  
+  <div class="container">
+    @foreach ($articles as $article)
+      <div>
+        <div class="card-body">
+          <h4 class="card-title">{{ $article->title }}</h4>
+<!--           <p class="card-text">{{ $article->body }}</p> -->
+        </div>
+      </div>
 @endsection
